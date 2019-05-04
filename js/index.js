2b6708c9907cb8d0579a0ec45c0be9b0f78c3644
Matt Hagner <web@matthagner.com>
2019-05-04 09:54:20 -0500

    Add window load event listener

--- js/index.js
+++ js/index.js
@@ -12,4 +12,10 @@ const preventDefault = (element, eventName) => {
 const navItems = document.querySelectorAll('nav a');
 
 /* Events / Event handlers */
+// Nav item prevent default
 navItems.forEach(item => preventDefault(item, 'click'));
+
+// Event 1. load
+window.addEventListener('load', () => {
+  alert("The page has loaded")
+})
