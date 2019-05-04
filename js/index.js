c2740032ddfb70b6c772eaac0361532d74bfe55b
Matt Hagner <web@matthagner.com>
2019-05-04 09:47:49 -0500

    Prevent navigation links from refreshing page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+
+/* Helper Functions */
+const preventDefault = (element, eventName) => {
+  element.addEventListener(eventName, (e) => {
+    e.preventDefault();
+  })
+}
+
+/* Selectors */
+const navItems = document.querySelectorAll('nav a');
+
+/* Events / Event handlers */
+navItems.forEach(item => preventDefault(item, 'click'));
